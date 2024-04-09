246039de3623352bd965827a1e0af95c1f703621
Shreha Chowdhury <72803653+shrehaa@users.noreply.github.com>
2024-04-09 12:41:41 +0530

    cosmetic changes

--- constants/constants.js
+++ constants/constants.js
@@ -1,5 +1,4 @@
 import react from "../public/reactjs.png";
-import redux from "../public/redux.png";
 import typeScript from "../public/typeScript.png";
 import JavaScript from "../public/javascript.png";
 import html from "../public/html.png"
@@ -14,14 +13,28 @@ import hub from "../public/github.png"
 
 
 export const items = ["About", "Skills", "Projects", "Contact"];
+
+const reduxURL ="https://i.postimg.cc/rKbmYGxJ/redux.png";
+const reactURL = "https://i.postimg.cc/2303nN5g/reactjs.png"
+const muiURL = "https://i.postimg.cc/LJ1XdqVM/mui.png"
+const javascriptURL = "https://i.postimg.cc/jnS5BZM8/javascript.png"
+
 export const skills = [
   {
     name: "react",
-    icon: react,
+    icon: reactURL,
   },
   {
     name: "JavaScript",
-    icon: JavaScript,
+    icon: javascriptURL,
+  },
+  {
+    name:"Redux",
+    icon:reduxURL
+  },
+  {
+    name:"TypeScript",
+    icon:"https://i.postimg.cc/R3x0vMPJ/typescript.png"
   },
   {
     name: "HTML",
@@ -37,24 +50,25 @@ export const skills = [
   },
   {
     name: "GIT",
-    icon: git,
+    icon: "https://i.postimg.cc/475y8rQ7/git.png",
   },
   ,
   {
     name: "Github",
-    icon: hub,
+    icon: "https://i.postimg.cc/s1jvBg3p/github.png",
   },
   {
     name: "Material Ui",
-    icon: mui,
+    icon: muiURL,
   },
+  
 ];
 export const projects = [
   {
     name: "Grains & Greens",
     decription:
       "A food delivery app which provides various options like wide range of catergories, scroll the catalog, filter as per mood, search an item, add to cart & extremely healthy food options to the user",
-    techstack: [react, JavaScript, mui],
+    techstack: [reactURL, javascriptURL, muiURL],
     icon: gg,
     demo: "https://grainsandgreens.netlify.app/",
     code: "https://github.com/shrehaa/grains-green",
@@ -63,20 +77,23 @@ export const projects = [
     name: "Streamix",
     decription:
       "A movie suggestion app for people to find various movies, GPT Search, using the latest GPT-3.5 Turbo API, with  Multi-Language support and advanced data handling with Redex Toolkit.",
-    techstack: [react, redux, JavaScript, mui],
+    techstack: [reactURL, reduxURL, javascriptURL, muiURL],
     icon: flix,
     demo: "https://streamixio.netlify.app/",
     code: "https://github.com/shrehaa/flix-pro",
   },
   {
     name: "Weekend Wanderer",
-    decription: "COMING SOOOOOOON",
-    techstack: [react, redux, typeScript, mui],
+    decription:
+      "A travel guide app where you can book you vacation packages, explore options for your travel from trekking to luxury trips, find group you can tag yourself along with, find various itineries and much more with GPT 3.5.",
+    techstack: [reactURL, reduxURL, javascriptURL, muiURL],
     icon: ww,
-    demo: "",
-    code: "",
+    demo: "https://streamixio.netlify.app/",
+    code: "https://github.com/shrehaa/flix-pro",
   },
 ];
+
+//A travel guide app where you can book you vacation packages, explore options for your travel from trekking to luxury trips, find group you can tag yourself along with, find various itineries and much more with GPT 3.5
 
 
 export const env = import.meta.env.VITE_REACT_ENVIRONMENT;
